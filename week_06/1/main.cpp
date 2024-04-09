4401517d9343f4703e2a2cc7670dde7529a5b252
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-04-09 15:38:59 +0300

    format

--- week_06/1/main.cpp
+++ week_06/1/main.cpp
@@ -4,17 +4,17 @@
 
 int main() {
 	std::cout << "HELLO, FMI!!" << std::endl;
-	
+
 	ArrayFile myNums("array.bin");
 
-	if(!myNums.didRead()) {
+	if (!myNums.didRead()) {
 		std::cout << "INITIALIZING..." << std::endl;
-		for(std::size_t i = 0; i < myNums.getSize(); ++i) {
+		for (std::size_t i = 0; i < myNums.getSize(); ++i) {
 			myNums[i] = i * i * i * std::sin(i / 100.f);
 		}
 	}
 
-	for(std::size_t i = 0; i < myNums.getSize(); ++i) {
+	for (std::size_t i = 0; i < myNums.getSize(); ++i) {
 		std::cout << myNums[i] << " ";
 	}
 	std::cout << std::endl;
