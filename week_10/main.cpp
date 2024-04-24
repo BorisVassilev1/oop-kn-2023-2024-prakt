a456e18c29d880e618dcedc752eb5cd6f048f493
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-04-24 14:04:59 +0300

    week 10 materials

---
+++ week_10/main.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <iostream>
+
+enum AnimalType {
+	BASE,
+	DOG, CAT
+};
+
+void makeSoundCat() {std::cout << "meow" << std::endl;};
+void makeSoundDog() {std::cout << "bow" << std::endl;};
+void (*vtable_makeSound[])(void) = {
+	nullptr,
+	makeSoundCat, makeSoundDog
+};
+
+class Animal {
+public:
+	AnimalType type;
+	Animal(AnimalType type) : type(type) {}
+	void makeSound() {
+		vtable_makeSound[type]();
+	};
+};
+
+class Cat : public Animal {
+public:
+	Cat() : Animal(CAT) {}
+};
+
+class Dog : public Animal {
+public:
+	Dog() : Animal(DOG) {}
+};
+
+
+int main() {
+	Animal* d = new Dog();
+	Animal* c = new Cat();
+	
+	d->makeSound();
+	c->makeSound();
+	return 0;
+}
