3ec12e19acb8531a025aab3ec6d73e9984d24643
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-05-20 12:54:03 +0300

    week 10 dixes

--- week_10/rpg.cpp
+++ week_10/rpg.cpp
@@ -13,46 +13,46 @@ enum class AttackType : int {
 /// base Character class
 class Character {
    public:
-	virtual void takeDamage(int power, AttackType type){};
+	virtual void takeDamage(int, AttackType){};
 	virtual bool isAlive() { return false; }
-	virtual void attack(Character *other) {}
+	virtual void attack(Character *) {}
 };
 
 /// uuuurgh
-class Zombie {
+class Zombie : public Character {
    public:
 	int	 health = 20;
-	void takeDamage(int power, AttackType type) {
+	void takeDamage(int power, AttackType type) override {
 		int modifier = 1 + (type == AttackType::FIRE);
 		health		 = std::max(health - power * modifier, 0);
 	}
-	bool isAlive() { return health > 0; }
-	void attack(Character *other) { other->takeDamage(2, AttackType::PHYSICAL); }
+	bool isAlive() override { return health > 0; }
+	void attack(Character *other) override { other->takeDamage(2, AttackType::PHYSICAL); }
 };
 
 /// player with three lives
-class Player {
+class Player : public Character {
 	int health = 50;
 	int lives  = 3;
 
    public:
-	void takeDamage(int power, AttackType type) {
+	void takeDamage(int power, AttackType) override {
 		health = std::max(health - power, 0);
 		if (health == 0 && lives) {
 			--lives;
 			health = 50;
 		}
 	}
-	bool isAlive() { return health > 0; }
-	void attack(Character *other) { other->takeDamage(4, AttackType(rand() % int(AttackType::COUNT))); }
+	bool isAlive() override { return health > 0; }
+	void attack(Character *other) override { other->takeDamage(4, AttackType(rand() % int(AttackType::COUNT))); }
 };
 
 /// a creature from the depths of Hell!! Does not like water.
-class FireElemental {
+class FireElemental : public Character {
 	int health = 30;
 
    public:
-	void takeDamage(int power, AttackType type) {
+	void takeDamage(int power, AttackType type) override {
 		float modifier = 1;
 		if (type == AttackType::FIRE) {
 			modifier = -0.5f;
@@ -61,17 +61,24 @@ class FireElemental {
 		}
 		health = std::max(health - int(power * modifier), 0);
 	}
-	bool isAlive() { return health > 0; }
-	void attack(Character *other) { other->takeDamage(8, AttackType::FIRE); }
+	bool isAlive() override { return health > 0; }
+	void attack(Character *other) override { other->takeDamage(8, AttackType::FIRE); }
 };
 
 /// Yipee!
-class Goomba {
+class Goomba : public Character {
 	int alive = true;
 
    public:
-	void takeDamage(int, AttackType) { alive = false; }
-	bool isAlive() { return alive; }
-	void AttackType(Character *) {}
+	void takeDamage(int, AttackType) override { alive = false; }
+	bool isAlive() override { return alive; }
+	void attack(Character *) override {}
 };
+
+int main() {
+	Player steve;
+	Zombie dave;
+	FireElemental ragnaros;
+	Goomba mushroom;
+}
 
